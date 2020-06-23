8c79850112179eeed724c19bb4b6f25a3ef96b96
UmairAnwar317 <umairanwar317@gmail.com>
2020-06-23 23:49:12 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// chapter 21-25 task # 3
+
+//var country = "Pakistani" ;
+
+//console.log(country.indexOf("n"))
+ 
+//task # 4 
+
+//var country = "Hello World" ;
+
+//console.log(country.lastIndexOf("l"))
+
+//task # 5
+
+//var a = "Pakistani" ;
+//console.log(a.charAt(3))
+
+//task # 7
+
+//var city = "Hyderabad"
+//console.log(city.replace("Hyder","Islam"))
+
+//task # 8
+
+//var message = "“Ali and Sami are best friends. They play cricket and football together.”" ;
+
+//console.log(message.replace(/and/g,"&"))
+
+//task # 12
+
+//var num = 35.36;
+//console.log(num.toString())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
